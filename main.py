04304c4cd66cb0d3078ba9569108bf3dca2e27f0
HarshithRajesh <harshithr2004@gmail.com>
2023-06-20 23:57:35 +0530

    people detected

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import cv2
+import os
 
-image = cv2.imread('C:\\Users\\harsh\\Python Scripting\\Face Detectors\\humans.jpeg',1)
-face_cascade = cv2.CascadeClassifier('C:\\Users\\harsh\\Python Scripting\\Face Detectors\\faces.xml')
+CASCADE = 'C:\\Users\\harsh\\Python Scripting\\Face Detectors\\faces.xml'
+INPUT_FOLDER = 'C:\\Users\\harsh\\Python Scripting\\Face Detectors\\input'
+OUTPUT_FOLDER = 'C:\\Users\\harsh\\Python Scripting\\Face Detectors\\output'
+COLOUR = (255,255,255)
+WIDTH = 4
+SCALE = 1.1
+NEIGHBOURS = 4
 
-faces = face_cascade.detectMultiScale(image, 1.1, 4)
-print(faces)
+def has_face(image_path):
+    image = cv2.imread(image_path,1)
+    face_cascade = cv2.CascadeClassifier(CASCADE)
+    faces = face_cascade.detectMultiScale(image,SCALE,WIDTH)
+    print(faces)
 
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,255),4)
-cv2.imwrite('C:\\Users\\harsh\\Python Scripting\\Face Detectors\\humans.jpeg',image)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,255),4)
+    return image
+
+images = os.listdir(INPUT_FOLDER)
+for image in images:
+    face_image= has_face(f'{INPUT_FOLDER}\\{image}')
+    if face_image is not None:
+        cv2.imwrite(f'{OUTPUT_FOLDER}\\{image}',face_image)
